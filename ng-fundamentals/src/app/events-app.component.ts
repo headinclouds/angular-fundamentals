465259d7e9eca53b7f7a4d6dd82c657fa5f3b387
Deordiieva <olena.deordiieva@accenture.com>
2018-12-07 00:46:39 +0200

    Communicating Between Angular Components

--- ng-fundamentals/src/app/events-app.component.ts
+++ ng-fundamentals/src/app/events-app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app-root',
-  template: '<h1>Hello world</h1>',
+  template: '<events-list></events-list>',
 })
 export class EventsAppComponent {
   title = 'ng-fundamentals';
---
+++ ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+
+@Component({
+    selector: "event-thumbnail",
+    templateUrl: "./event-thumbnail.component.html"
+    
+})
+
+export class EventThumbnailComponent {
+    @Input() event:any;
+    @Output() eventClick = new EventEmitter();
+    handleClick() {
+        this.eventClick.emit(this.event.name);        
+    }
+}
