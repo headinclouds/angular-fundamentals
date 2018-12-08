059c59df9389beda843e9176bd9d8dc3d9f4fb80
Deordiieva <olena.deordiieva@accenture.com>
2018-12-08 02:26:26 +0200

    adding site header

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -4,12 +4,15 @@ import { NgModule } from '@angular/core';
 import { EventsAppComponent } from './events-app.component';
 import {EventsListComponent} from "./events/events-list.component";
 import {EventThumbnailComponent} from "./events/event-thumbnail.component";
+import { NavbarComponent } from './nav/navbar.component';
 
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListComponent,
-    EventThumbnailComponent
+    EventThumbnailComponent,
+    NavbarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
--- ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -2,14 +2,20 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
     selector: "event-thumbnail",
-    templateUrl: "./event-thumbnail.component.html"
+    templateUrl: "./event-thumbnail.component.html",
+    styles: [`h2 {margin-top: 10px}`]
     
 })
 
 export class EventThumbnailComponent {
     @Input() event:any;
     @Output() eventClick = new EventEmitter();
+    someProperty: any = "some value";
+
     handleClick() {
         this.eventClick.emit(this.event.name);        
+    }
+    logFoo(){
+        console.log('foo');
     }
 }
