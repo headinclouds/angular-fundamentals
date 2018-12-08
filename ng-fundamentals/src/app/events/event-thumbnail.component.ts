c83f4c9199a365167802cb7e47dc98fd60611849
Deordiieva <olena.deordiieva@accenture.com>
2018-12-09 01:42:03 +0200

    .grey{color: grey}

--- ng-fundamentals/src/app/events/event-thumbnail.component.ts
+++ ng-fundamentals/src/app/events/event-thumbnail.component.ts
@@ -3,19 +3,31 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 @Component({
     selector: "event-thumbnail",
     templateUrl: "./event-thumbnail.component.html",
-    styles: [`h2 {margin-top: 10px}`]
-    
+    styles: [`
+    h2 {margin-top: 10px}
+                .thumbnail {min-height: 240px}
+                .red {color: red}
+                .salad {color: lightgreen}
+                .grey{color: grey}
+                `]
+
 })
 
 export class EventThumbnailComponent {
-    @Input() event:any;
+    @Input() event: any;
     @Output() eventClick = new EventEmitter();
     someProperty: any = "some value";
 
     handleClick() {
-        this.eventClick.emit(this.event.name);        
+        this.eventClick.emit(this.event.name);
     }
-    logFoo(){
+    logFoo() {
         console.log('foo');
+    }
+    getColorStartTime() {
+        const early = this.event && this.event.time === '8:00 am';
+        const late = this.event && this.event.time === '10:00 am';
+        const normal = this.event && this.event.time === '9:00 am';
+        return { salad: early, red: late, grey: normal };
     }
 }
